2a302eb85702cb740133348cc335667a5ebf20ff
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2020-08-21 16:38:47 +0200

    Compatibility with PHPUNIT 9

--- Test/Unit/Service/SrcSet/GeneratorTest.php
+++ Test/Unit/Service/SrcSet/GeneratorTest.php
@@ -19,7 +19,7 @@ class GeneratorTest extends \PHPUnit\Framework\TestCase
      */
     protected $generator;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->objectManager = \Magento\TestFramework\ObjectManager::getInstance();
 
